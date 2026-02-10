91d8a8fc49a340912a50f2ab10819f500db6413a
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-02-10 13:40:16 +0200

    Add additional options for switching between tabs using buttons with keys, and also adapt this feature for MacOS

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -20,13 +20,28 @@ class Tabs {
     this.rootElement = rootElement;
     this.buttonElements = this.rootElement.querySelectorAll(this.selectors.button);
     this.contentElements = this.rootElement.querySelectorAll(this.selectors.content);
-    this.state = {
+    this.state = this.getProxyState({
       activeTabIndex: [...this.buttonElements].findIndex((buttonElement) => {
         buttonElement.classList.contains(this.stateClasses.isActive);
       }),
-    };
+    });
     this.limitTabsIndex = this.buttonElements.length - 1;
     this.bindEvents();
+  }
+
+  getProxyState(initialState) {
+    return new Proxy(initialState, {
+      get: (target, prop) => {
+        return target[prop];
+      },
+      set: (target, prop, value) => {
+        target[prop] = value;
+
+        this.updateUI();
+
+        return true;
+      },
+    });
   }
 
   updateUI() {
@@ -47,15 +62,69 @@ class Tabs {
     });
   }
 
+  activeTab(newTabIndex) {
+    this.state.activeTabIndex = newTabIndex;
+    this.buttonElements[newTabIndex].focus();
+  }
+
+  previousTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === 0 ? this.limitTabsIndex : this.state.activeTabIndex - 1;
+
+    this.activeTab(newTabIndex);
+  };
+
+  nextTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === this.limitTabsIndex ? 0 : this.state.activeTabIndex + 1;
+
+    this.activeTab(newTabIndex);
+  };
+
+  firstTab = () => {
+    this.activeTab(0);
+  };
+
+  lastTab = () => {
+    this.activeTab(this.limitTabsIndex);
+  };
+
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex;
-    this.updateUI();
   }
+
+  onKeyDown = (event) => {
+    const {code, metaKey} = event;
+
+    const action = {
+      ArrowLeft: this.previousTab,
+      ArrowRight: this.nextTab,
+      Home: this.firstTab,
+      End: this.lastTab,
+    }[code];
+
+    const isMacHomeKey = metaKey && code === "ArrowLeft";
+
+    if (isMacHomeKey) {
+      this.firstTab();
+      return;
+    }
+
+    const isMacEndKey = metaKey && code === "ArrowRight";
+
+    if (isMacEndKey) {
+      this.lastTab();
+      return;
+    }
+
+    action?.();
+  };
 
   bindEvents() {
     this.buttonElements.forEach((buttonElement, index) => {
       buttonElement.addEventListener("click", () => this.onButtonClick(index));
     });
+    this.rootElement.addEventListener("keydown", this.onKeyDown);
   }
 }
 
